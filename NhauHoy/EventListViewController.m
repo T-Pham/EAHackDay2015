d6fbb2a10718de681c7d15a5ccaf6d1ecf6e0473
Thanh Pham <minhthanh@me.com>
2015-08-08 21:41:05 +0700

    Implement form to enter new event

--- NhauHoy/EventListViewController.m
+++ NhauHoy/EventListViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Thanh Pham. All rights reserved.
 //
 
+#import <UIAlertView+Block.h>
+
 #import "EventListViewController.h"
 
 @interface EventListViewController ()
@@ -17,6 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Event List";
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addButtonTapped:)];
+}
+
+- (void)addButtonTapped:(id)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Create new event" message:@"Enter event name" cancelButtonTitle:@"Cancel" otherButtonTitle:@"Create"];
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alertView showUsingBlock:^(UIAlertView *alertView, NSInteger buttonIndex) {
+        if (buttonIndex == 1) {
+            NSLog(@"%@", [[alertView textFieldAtIndex:0] text]);
+        }
+    }];
 }
 
 @end
